de3a42b82af77d0605c1fddbab71e3a6ad80598b
RonSasson67 <ron6764527@gmail.com>
2023-08-11 03:48:46 +0300

    Add api calling for yahoo fainance

--- src/company-valuation/company-valuation.service.ts
+++ src/company-valuation/company-valuation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { MultipleValuationResponse } from './DTO/MultipleValuation/MultipleValuationResponse';
 
+
 const TOKEN = `chj8e69r01qh5480dvg0chj8e69r01qh5480dvgg`
 
 @Injectable()
@@ -10,16 +11,24 @@ export class CompanyValuationService {
 
     async MultipleValuation(symbol: string): Promise<MultipleValuationResponse> {    
         const url = `https://finnhub.io/api/v1/stock/metric?symbol=${symbol}&metric=ALL&token=${TOKEN}`;
-        const response = await fetch(url);
-        const data = await response.json();
-
-        console.log(data.metric);
+        const yahoourl = `https://finance.yahoo.com/quote/${symbol}/analysis`
         
+        const response = await fetch(url);
+        const yahooResponse = await fetch(yahoourl);
+
+        const data = await response.json();
+        const yahooData = await yahooResponse.text();
+
+        const textToFinde = `<span>Next 5 Years (per annum)</span></td><td class="Ta(end) Py(10px)">`
+        const indexOfGrowEstamite = yahooData.indexOf(textToFinde);
+        let GrowthRateInPrecent = (yahooData.substring(indexOfGrowEstamite + textToFinde.length , indexOfGrowEstamite + textToFinde.length + 10));
+        GrowthRateInPrecent = GrowthRateInPrecent.substring(0, GrowthRateInPrecent.indexOf('%'));
+
         const multipleValuationResponse: MultipleValuationResponse = {
             symbol: data.symbol,
             eps: data.metric.epsBasicExclExtraItemsAnnual,
             peRecomended: data.metric.peExclExtraTTM,
-            GrowthRateInPrecent: data.metric.epsGrowth5Y
+            GrowthRateInPrecent: parseFloat(GrowthRateInPrecent)
         };
 
         return multipleValuationResponse;
